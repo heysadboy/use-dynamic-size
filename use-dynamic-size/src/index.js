11929a42a08f8fdf10db5f3ebe81b3c9334ab4e9
Varun Kumar <heysadboy@gmail.com>
2021-03-07 21:02:11 +0900

    [use-custom-hook] Initialized Project

---
+++ use-dynamic-size/src/index.js
@@ -0,0 +1,1 @@
+export { default as useDynamicSize } from './useDynamicSize';
---
+++ use-dynamic-size/src/useDynamicSize.js
@@ -0,0 +1,6 @@
+export default function useDynamicSize (settings = {}) {
+  const { message } = settings;
+  return {
+    message
+  };
+}
